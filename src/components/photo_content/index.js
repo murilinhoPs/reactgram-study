2fe4d1e4e7e36af08936d088a987fa3e974dfc32
murilinhoPs <murilinhops@hotmail.com>
2020-04-13 13:37:08 -0300

    finished the main design of the card.

---
+++ src/components/photo_content/index.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { BsThreeDots, BsBookmark, BsHeartFill, BsHeart } from "react-icons/bs";
+import { FaRegCommentDots, FaRegPaperPlane } from "react-icons/fa";
+
+import "./styles.css";
+
+const Photo = ({ url, legend, name, profile_image, profile_link }) => {
+  let curtir = false;
+
+  const [state, setState] = useState({ curtiu: curtir });
+
+  const changeIcon = () => {
+    setState({ curtiu: !state.curtiu });
+
+    console.log("changeIcon");
+  };
+
+  const PhotoHeader = () => (
+    <div className="Photo-header">
+      <div className="Profile">
+        <div className="Profile-photo">
+          <img src={profile_image} alt="Foto de perfil" />
+        </div>
+        <div className="Profile-name">
+          <a href={profile_link} target="blank">
+            {name}
+          </a>
+          <text>São Paulo, Brazil</text>
+        </div>
+      </div>
+      <BsThreeDots size="22px" opacity="0.7" />
+    </div>
+  );
+  const FooterIcons = () => (
+    <div className="footer-icons">
+      <div className="primary-icons">
+        <div onClick={changeIcon} className="icon-click-event">
+          {!state.curtiu && <BsHeart size="23px" />}
+
+          {state.curtiu && (
+            <span>
+              <BsHeartFill color="#FF0000" size="23px" />
+            </span>
+          )}
+        </div>
+        <FaRegCommentDots size="19px" opacity="0.5" />
+        <FaRegPaperPlane size="19px" opacity="0.5" />
+      </div>
+      <BsBookmark size="23px" opacity="0.7" />
+    </div>
+  );
+
+  const FooterNewComment = () => (
+    <div className="Photo-new-comment">
+      <form className="Adicione">
+        <input></input>
+      </form>
+    </div>
+  );
+
+  return (
+    <div className="Photo">
+      <PhotoHeader />
+
+      <div className="Photo-container">
+        <img src={url} alt="Foto" />
+      </div>
+
+      <div className="Photo-footer">
+        <FooterIcons />
+        {state.curtiu && (
+          <div className="likes">
+            <text>Curtido por: {name}</text>
+          </div>
+        )}
+
+        <legend>
+          {name} <text>{legend}</text>
+        </legend>
+      </div>
+    </div>
+  );
+};
+
+export default Photo;
---
+++ src/pages/App.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./App.css";
+
+import Photo from "../components/photo_content/index";
+
+function App() {
+  const myProfileData = {
+    src: "https://miro.medium.com/max/3840/1*ivjksIhvAs7TUXbQCxAU0A.jpeg",
+    user: {
+      name: "murilinhops",
+      profile_image:
+        "https://instagram.fcgh11-1.fna.fbcdn.net/v/t51.2885-19/s150x150/72150440_585225525552082_8166883981246070784_n.jpg?_nc_ht=instagram.fcgh11-1.fna.fbcdn.net&_nc_ohc=78HqPpLlpCEAX-WNNdk&oh=dd13ac749a41246741f1d17720cb438c&oe=5EBCB808",
+      link: "https://www.instagram.com/murilinhops/",
+    },
+    legend: "Treinando ReactJs",
+  };
+
+  const myFlutterData = {
+    src:
+      "https://raw.githubusercontent.com/murilinhoPs/challenge_nike_app/master/images/animated.jpg",
+    legend: "Treinando design e animações em flutter",
+  };
+
+  const gitHubData = {
+    src: "https://i.imgur.com/0z0VEYF.png",
+    user: {
+      name: "github",
+      profile_image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSTsjEwUaj7SjHQ4Encep0VJOz-ODloafMTUOPLi0Wg_x5wsg3q&usqp=CAU",
+      link: "https://www.instagram.com/github/",
+    },
+    legend: "Open collaboration on COVID-19",
+  };
+
+  return (
+    <div className="App">
+      <Photo
+        url={myProfileData.src}
+        legend={myProfileData.legend}
+        name={myProfileData.user.name}
+        profile_image={myProfileData.user.profile_image}
+        profile_link={myProfileData.user.link}
+      />
+      <Photo
+        url={gitHubData.src}
+        legend={gitHubData.legend}
+        name={gitHubData.user.name}
+        profile_image={gitHubData.user.profile_image}
+        profile_link={gitHubData.user.link}
+      />
+      <Photo
+        url={myFlutterData.src}
+        legend={myFlutterData.legend}
+        name={myProfileData.user.name}
+        profile_image={myProfileData.user.profile_image}
+        profile_link={myProfileData.user.link}
+      />
+    </div>
+  );
+
+  // return (
+  //   <div className="App">
+  //     <Photo {...imageData} />
+  //   </div>
+  // );
+}
+
+export default App;
